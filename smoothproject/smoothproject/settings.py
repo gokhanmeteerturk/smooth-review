e478d56847f6002faa83187184249a8b74ba720f
gokhanmeteerturk <me@e-mete.com>
2022-10-12 23:12:57 +0300

    local setting for app

--- smoothproject/smoothproject/settings.py
+++ smoothproject/smoothproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'smoothapp.apps.SmoothappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
