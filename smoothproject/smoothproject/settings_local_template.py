2d6c6fa25b02bff7657fbd1fc784f80c566c3a0a
gokhanmeteerturk <me@e-mete.com>
2022-10-12 22:23:12 +0300

    local settings added

---
+++ smoothproject/smoothproject/settings_local_template.py
@@ -0,0 +1,13 @@
+ALLOWED_HOSTS = ['*']
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '...',
+        'USER': '...',
+        'PASSWORD': '...',
+        'HOST': '',
+        'PORT': ''
+    }
+}
+SECRET_KEY = '...............'
--- smoothproject/smoothproject/urls.py
+++ smoothproject/smoothproject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('smoothapp/', include('smoothapp.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('smoothapp.urls')),
+    path('yonet/', admin.site.urls),
 ]
