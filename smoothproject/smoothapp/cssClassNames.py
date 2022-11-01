c3b6d245c62f9d5f980221a189fd8820ff2f60f0
gokhanmeteerturk <me@e-mete.com>
2022-11-01 23:46:26 +0300

    New custom tag and css class name provider

---
+++ smoothproject/smoothapp/cssClassNames.py
@@ -0,0 +1,4 @@
+class FormCss:
+    text_input_class_name = 'form-control'
+    file_input_class_name = 'form-control form-control-md'
+    big_submit = 'btn btn-dark btn-block py-2 mb-4 mt-5 font-weight-bold'
--- smoothproject/smoothapp/templatetags/custom_tags.py
+++ smoothproject/smoothapp/templatetags/custom_tags.py
@@ -1,12 +1,28 @@
 from django import template
 from django.conf import settings
+import random
+from smoothapp import cssClassNames
 
 register = template.Library()
 
 ALLOWED_SETTINGS = ('SITENAME_PRETTY', 'DEFAULT_FROM_EMAIL')
+
 
 @register.simple_tag
 def get_setting(name):
     if name not in ALLOWED_SETTINGS:
         return ""
     return getattr(settings, name, "")
+
+
+@register.simple_tag
+def get_class_name(css_group_name, attribute_name):
+    css_group = getattr(cssClassNames, css_group_name, None)
+    if css_group:
+        return getattr(css_group, attribute_name, "")
+    return ""
+
+
+@register.simple_tag
+def get_random_category():
+    return random.choice(['game', 'shoes', 'burger', 'drink', 'game', 'car', 'game'])
